876c200e3292900864e4f2c81964bc00011330c7
khadijaiqbal00 <82168872+khadijaiqbal00@users.noreply.github.com>
2023-08-01 19:46:24 +0500

    Project Structure

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import DocLogin from './src/Screens/Doctor-Login/DocLogin'
+
+const App = () => {
+  return (
+    <DocLogin/>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
---
+++ src/Screens/Doctor-Login/DocLogin.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const DocLogin = () => {
+  return (
+    <View style={styles.container1}>
+    
+    </View>
+  )
+}
+
+export default DocLogin
+
+const styles = StyleSheet.create({
+    container1:{
+        flex:1,
+        backgroundColor:'#DCEDF9',
+    },
+    container2:{
+        padding:30,
+        marginTop:40,
+    }
+})
