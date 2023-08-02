d3bfe7611390256072d29933c6093fc4c2341ae6
khadijaiqbal00 <82168872+khadijaiqbal00@users.noreply.github.com>
2023-08-02 12:44:09 +0500

    Working on Login Screen

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import 'react-native-gesture-handler';
 import {StyleSheet, Text, View, StatusBar} from 'react-native';
 import React from 'react';
 import Root from './src/Navigation/Root';
+import DocLogin from './src/Screens/DocLogin';
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <StatusBar barStyle={'light-content'} backgroundColor='black' /> */}
-      <Root />
+      {/* <Root /> */}
+      <DocLogin/>
     </View>
   );
 }
--- src/Screens/DocLogin.js
+++ src/Screens/DocLogin.js
@@ -8,79 +8,103 @@ import {
   ScrollView,
 } from 'react-native';
 import React from 'react';
+import {colors} from '../global/globalstyles';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import * as Animatable from 'react-native-animatable';
-
-export default function DocLogin({navigation}) {
+import {IconButton} from 'react-native-paper';
+import {color} from 'react-native-reanimated';
+import {red100} from 'react-native-paper/lib/typescript/src/styles/themes/v2/colors';
+export default function LogIn({navigation}) {
   return (
     <View style={styles.Container}>
-      <ScrollView>
-        <Text style={styles.text}>Welcome to </Text>
-        <View>
-          <Image
-            source={{
-              uri: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fHww&w=1000&q=80',
-            }}
-            style={{
-              height: 50,
-              width: 220,
-              marginTop: '5%',
-              alignSelf: 'center',
-            }}
-          />
-        </View>
+<ScrollView>
 
-        <View>
-          <TextInput
-            placeholderTextColor="grey"
-            placeholder="Enter your email"
-            style={[styles.TextInput, {text: 'black'}]}></TextInput>
-        </View>
-        <View style={[styles.TextInput, {marginTop: '5%'}]}>
-          <TextInput
-            style={styles.text2}
-            placeholderTextColor="grey"
-            placeholder="Enter your password"
-            secureTextEntry={true}></TextInput>
-          <Animatable.View animation={'fadeInDown'} duration={400}>
-            <MaterialIcons
-              name="visibility-off"
-              style={{
-                color: 'grey',
-                alignSelf: 'flex-end',
-                marginRight: '5%',
-                marginTop: -37,
+<View style={{ flex: 1,
+  }}> 
+<IconButton
+        icon="chevron-left"
+        size={30}
+        onPress={() => navigation.goBack()}
+      />
+
+      
+        <Text style={styles.text}>Log in as a Doctor </Text>
+        <Text style={styles.text1}>
+          Please fill in the information below to login to your account.
+        </Text>
+</View>
+
+
+  
+<View style={styles.container2}>
+          <View style={styles.inside}>
+            <View style={styles.box1}>
+              <Text style={styles.text6}>E-mail </Text>
+              <TextInput
+                placeholder="Enter your email"
+                style={styles.TextInput}></TextInput>
+            </View>
+            <View style={styles.box2}>
+              <Text style={styles.text6}>Password </Text>
+              <View style={[styles.TextInput]}>
+                <TextInput
+                  style={styles.text2}
+                  placeholder="Enter your password"
+                  secureTextEntry={true}></TextInput>
+                <Animatable.View animation={'fadeInDown'} duration={400}>
+                  <MaterialIcons
+                    name="visibility-off"
+                    style={{
+                      color: 'grey',
+                      alignSelf: 'flex-end',
+                      marginRight: '5%',
+                      marginTop: -30,
+                    }}
+                    size={20}
+                  />
+                </Animatable.View>
+              </View>
+            </View>
+            
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('ForgotPassword');
+              }}>
+              <Text style={styles.text3}> Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => {
+                // navigation.navigate('DrawerNavigation');
               }}
-              size={20}
-            />
-          </Animatable.View>
+              style={styles.btnShape}>
+              <Text style={styles.btnText}>Login</Text>
+            </TouchableOpacity>
+
+
+
+            <View style={styles.bottomLine}>
+              <Text style={styles.text4}>Don’t have an account?</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('SignUp');
+                }}>
+                <Text style={styles.text5}>Register Now</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.line}>
+            <View style={styles.line2}>
+            </View>
+            </View>
+
+            <View style={{marginBottom:'20%'}} >
+            </View>
+          </View>
         </View>
+      
 
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('ForgotPassword');
-          }}>
-          <Text style={styles.text3}> Forgot Password?</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('TabNavigation');
-          }}
-          style={styles.btnShape}>
-          <Text style={styles.btnText}>Login</Text>
-        </TouchableOpacity>
-
-        <View style={styles.bottomLine}>
-          <Text style={styles.text4}>Don’t have an account?</Text>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('Docsignup');
-            }}>
-            <Text style={styles.text5}>Register Now</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+   
+</ScrollView>
     </View>
   );
 }
@@ -88,26 +112,62 @@ export default function DocLogin({navigation}) {
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: colors.pageBackground,
+  },
+  container2: {
+    borderWidth: 1,
+    borderColor: colors.black1,
+    marginHorizontal: '9%',
+    marginVertical: '10%',
+    borderRadius: 5,
   },
   text: {
     alignSelf: 'center',
-    fontSize: 26,
+    fontSize: 20,
     marginTop: '20%',
-    color: "grey",
-    fontWeight: '700',
+    color: colors.black,
+    fontWeight: '900',
+  },
+  line: {
+    height: 1,
+    width: '87%',
+    alignSelf: 'center',
+    backgroundColor: 'rgba(179, 186, 198, 1)',
+  },
+  line2: {
+    height: 3,
+    marginTop:-1,
+    borderRadius:10,
+    width: '37%',
+    alignSelf: 'center',
+    backgroundColor: 'rgba(0, 96, 247, 1)',
+  },
+  text1: {
+    textAlign: 'center',
+    fontSize: 13,
+    color: colors.black,
+    fontWeight: '900',
+    marginHorizontal: '10%',
+    marginTop: 10,
+  },
+  box1: {
+    width: '100%',
+    marginTop: '15%',
+  },
+  box2: {
+    width: '100%',
+    marginTop: '3%',
   },
   TextInput: {
     alignSelf: 'center',
-    width: '80%',
-    backgroundColor: '#F7F8F9',
+    width: '87%',
+    backgroundColor: colors.white,
     borderRadius: 8,
-    marginTop: '15%',
-    height: 56,
+    height: 47,
     paddingLeft: 20,
     paddingTop: 5,
     fontFamily: 'Urbanist',
-    color: 'black',
+    color: '#8391A1',
     fontWeight: '100',
     fontSize: 15,
   },
@@ -118,44 +178,55 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
   text3: {
-    alignSelf: 'flex-end',
-    marginRight: '10%',
+    alignSelf: 'flex-start',
+    width: '87%',
+    alignSelf: 'center',
     marginTop: '2%',
-    color: '#6A707C',
+    color: colors.blue,
     fontFamily: 'Urbanist',
-    fontWeight: '600',
+    fontWeight: '900',
   },
   btnShape: {
-    backgroundColor: "black",
-    height: 50,
-    width: '80%',
+    backgroundColor: colors.black1,
+    height: 47,
+    width: '87%',
     alignSelf: 'center',
-    marginTop: '15%',
+    marginTop: '10%',
     borderRadius: 8,
+    backgroundColor: colors.darkblue,
   },
   btnText: {
     alignSelf: 'center',
-    fontSize: 21,
+    fontSize: 17,
     marginTop: 10,
-    color: "white",
-    fontWeight: '400',
+    color: colors.white,
+    fontWeight: '900',
   },
   bottomLine: {
     flexDirection: 'row',
     alignSelf: 'center',
-    marginTop: '40%',
+   marginTop:'3%',
+   marginBottom:'10%'
   },
   text4: {
-    fontWeight: '500',
-    color: '#1E232C',
+    fontWeight: '900',
+    color: '#757F8E',
     fontFamily: 'Urbanist',
     fontSize: 13,
   },
   text5: {
-    fontWeight: '700',
-    color: "black",
+    fontWeight: '900',
+    color: '#0060F7',
     marginLeft: 5,
     fontFamily: 'Urbanist',
     fontSize: 14,
+  },
+  text6: {
+    fontSize: 14,
+    fontWeight: '900',
+    color: colors.black,
+    width: '87%',
+    alignSelf: 'center',
+    marginBottom: 5,
   },
 });
