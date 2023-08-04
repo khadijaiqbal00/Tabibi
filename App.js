906bf1363ec7423a14d648b4b769965af26087ae
khadijaiqbal00 <82168872+khadijaiqbal00@users.noreply.github.com>
2023-08-04 19:11:20 +0500

    Doctor Profile Screen

--- App.js
+++ App.js
@@ -2,20 +2,14 @@ import 'react-native-gesture-handler';
 import {StyleSheet, Text, View, StatusBar} from 'react-native';
 import React from 'react';
 import Root from './src/Navigation/Root';
-import VideoCall1 from './src/Screens/Doctor/VideoCall1';
-import VoiceCall from './src/Screens/Doctor/VoiceCall';
-// import DocLogin from './src/Screens/DocLogin';
-// import DocSignUp from './src/Screens/DocSignUp';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={'light-content'} backgroundColor='black' /> 
-       {/* <Root /> */}
-      {/* <DocLogin/> */}
-      {/* <VideoCall1/>
-       */}
-       <VoiceCall/>
+       <Root />
+      
     </View>
   );
 }
